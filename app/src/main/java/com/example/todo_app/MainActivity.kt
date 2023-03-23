00b3e85c93ff00c9c670548f58874f3c0409ced2
Adam Černý <Adam.Cerny@gls-czech.com>
2023-03-23 16:15:16 +0100

    Completed bottom view menu

--- app/src/main/java/com/example/todo_app/MainActivity.kt
+++ app/src/main/java/com/example/todo_app/MainActivity.kt
@@ -2,12 +2,41 @@ package com.example.todo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.todo_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
 
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            when (it.itemId) {
+
+                R.id.home -> replaceFragment(Home())
+                R.id.home2 -> replaceFragment(Home2())
+                R.id.settings -> replaceFragment(Settings())
+
+                else -> {
+
+                }
+
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment (fragment : Fragment)
+    {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
 
     }
+
 }
